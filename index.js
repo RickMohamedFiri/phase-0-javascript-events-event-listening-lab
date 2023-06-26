89e7e45c970041f9f62f1961a85cb78952fc5905
Rick Mohamed <rickmohaa@gmail.com>
2023-06-26 20:01:30 +0300

    new commit

--- index.js
+++ index.js
@@ -1,2 +1,54 @@
-function addingEventListener() {
+
+let input = document.getElementById('input');
+
+
+const handlecallBack=()=>{
+    alert('I was clicked')      
 }
+
+function addingEventListener() {
+    input.addEventListener('click', handlecallBack)
+}
+
+addingEventListener();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
